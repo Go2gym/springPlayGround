5ece32bdedace5f303091af8055ae52208fccc50
Boiling SODA <go2gym365@gmail.com>
2023-09-07 11:50:39 +0000

    Member Controller

--- hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     
+    //컨트롤러가 우선순위가 더 높아서 먼저 찾고 없으면 static에서 찾음
     @GetMapping("/")
     public String home() {
         return "home";
