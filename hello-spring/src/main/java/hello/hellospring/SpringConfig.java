511bfe41b0f3a2d83a18c33a00877d7b7586713c
Boiling SODA <go2gym365@gmail.com>
2023-09-08 13:49:57 +0000

    Spring DataBase

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,36 +1,51 @@
 package hello.hellospring;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
     
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
+    
+    @Autowired //생성자가 하나인 경우 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }*/
+
+    /*private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
-    }
+    }*/
     
     //서비르랑 레포지토리를 스프링빈에 등록을 하고
     // 등록되어있는걸 멤버 서비스에 넣어줌
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean 
+    /*@Bean 
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+    }*/
 }
